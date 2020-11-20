265474fb5596cba161dedf805330fff8c0bd34ae
isabeldc1308 <isabeldc1308@gmail.com>
2020-11-20 14:46:27 -0500

    Segundo

---
+++ ProyectoCristian/wwwroot/usuario/js/alerta.js
@@ -0,0 +1,11 @@
+﻿let carrito= () => {
+    let carrito = document.getElementById("carrito");
+    let articulos = localStorage.getItem("staplesbmincart");
+    let n_articulos = articulos.length;
+    if (n_articulos != 0) {
+        console.log(n_articulos);
+    }
+    console.log("el que no funciono eres tu");
+};
+carrito();
+    
